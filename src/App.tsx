09390b5d5f716f20b89f045a0850616d31ad205a
thakuhuni <36413887+thakuhuni@users.noreply.github.com>
2022-06-30 22:13:54 +1000

    2nd file

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { MuiNavBar } from './components/MuiNavBar';
+import {DrawerAppBar} from './components/Props';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <DrawerAppBar/>
+    
+    </div>
+  );
+}
+
+export default App;
